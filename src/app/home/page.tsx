f35ef699fe82641e7726fa7c6553c24bc6688500
Betül Demir <81586476+betuldemirr@users.noreply.github.com>
2024-06-04 14:38:17 +0300

    Update page.tsx

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -22,7 +22,7 @@ const Home: React.FC = () => {
   return (
     <Container fluid className='home-container'>
       <Sidebar />
-      <div>
+      <div className='w-100'>
       <Container className='my-5'>
         <Alert variant="info" dismissible onClose={onCloseAlert}>
           Special Offer! Get Complete Free Proxy 10 MB Proxy, without credit card. <span className='text-decoration-underline'>Start Free Trial</span>
