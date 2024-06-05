86e90fac24c9e1e8c48f89db2d460d42d91d96a4
betuldemirr <betuldemir.007@gmail.com>
2024-06-05 14:45:30 +0300

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -22,17 +22,17 @@ const Home: React.FC = () => {
   return (
     <Container fluid className='home-container'>
       <Sidebar />
-      <div>
-      <Container className='my-5'>
-        <Alert variant="info" dismissible onClose={onCloseAlert}>
-          Special Offer! Get Complete Free Proxy 10 MB Proxy, without credit card. <span className='text-decoration-underline'>Start Free Trial</span>
-        </Alert>
-        <h1 className='my-5'>Proxies & Scraping Infrastructure</h1>
-      </Container>
-      <Navigation activeTab={activeTab} onTabChange={handleTabChange} />
-      {activeTab === 'dashboard' ? <Dashboard /> : <MyProxies />}
+      <div className='w-100'>
+        <Container className='my-5'>
+          <Alert variant="info" dismissible onClose={onCloseAlert}>
+            Special Offer! Get Complete Free Proxy 10 MB Proxy, without credit card. <span className='text-decoration-underline'>Start Free Trial</span>
+          </Alert>
+          <h1 className='my-5'>Proxies & Scraping Infrastructure</h1>
+        </Container>
+        <Navigation activeTab={activeTab} onTabChange={handleTabChange} />
+        {activeTab === 'dashboard' ? <Dashboard /> : <MyProxies />}
       </div>
-      
+
     </Container>
   );
 }
